de1c5980bae4ec3081cdd85dba2356179a8f5c88
Oscar Tong <moscartong@gmail.com>
2017-11-24 16:46:15 +0800

    switch to my unity-package-extract fork

--- test.js
+++ test.js
@@ -2,29 +2,16 @@ const _         = require('underscore'),
       fs        = require('fs'),
       fse       = require('fs-extra'),
       fsPath    = require('fs-path'),
+      path      = require('path'),
       Client    = require('unity-package-extract').UnityExtractClient;
 
-let client          = new Client(),
-    name            = 'VRTK - Virtual Reality Toolkit - VR Toolkit',
-    unitypackage    = `${__dirname}/.downloads/${name}.unitypackage`,
-    output          = `${__dirname}/.downloads/${name}/`,
-    tmp             = `${__dirname}/.downloads/_${name}/`;
+let client  = new Client(),
+    folder  = `./.downloads`,
+    name    = 'VRTK - Virtual Reality Toolkit - VR Toolkit',
+    unitypackage    = path.join(__dirname, folder, `${name}.unitypackage`),
+    output          = path.join(__dirname, folder, name);
 
-fse.emptyDirSync(tmp);
 fse.emptyDirSync(output);
-client.extract(unitypackage, tmp)
-    .then(() => {
-        _.each(fs.readdirSync(tmp), item => {
-            if(!item.startsWith('.')) {
-                let path = fs.readFileSync(`${tmp}${item}/pathname`).toString().split('\n')[0],
-                    newPath = `${output}${path}`,
-                    oldPath = `${tmp}${item}/asset`;
-                if(fs.existsSync(oldPath)) {
-                   fse.moveSync(oldPath, newPath, {overwrite:true});
-                }
-            }
-        });
-        fse.removeSync(tmp);
-    })
+client.extract(unitypackage, output)
     .then(() => console.log('done'))
     .catch(console.error);
